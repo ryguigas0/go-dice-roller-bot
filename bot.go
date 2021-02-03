7cbcecfee3abdcd26ef779b9240ecf22514e2357
ryguigas <gui6479@gmail.com>
2021-02-03 10:45:52 -0300

    Converted bot from js to golang

---
+++ bot.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"os/signal"
+	"strconv"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+// Variables used for command line parameters
+var (
+	Token string
+)
+
+func init() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println("ERROR LOADING .env FILE")
+	}
+	Token = os.Getenv("TOKEN")
+}
+
+func main() {
+
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	dg.AddHandler(messageCreate)
+
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	if err = dg.Open(); err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	dg.Close()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	if m.Author.ID == s.State.User.ID || m.Author.Bot {
+		return
+	}
+
+	if strings.Join(strings.Split(m.Content, "")[:2], "") != "&d" {
+		return
+	}
+
+	num, err := strconv.Atoi(strings.Join(strings.Split(m.Content, "")[3:], ""))
+	if err != nil || num <= 0 {
+		s.ChannelMessageSend(m.ChannelID, "Por favor escreva um número (&d [número])")
+		return
+	}
+	rand.Seed(time.Now().UnixNano())
+	randNum := rand.Intn(num-1) + 1
+	fmt.Printf("%s : %v : %v", m.Author, num, randNum)
+	s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%v tirou %v no d%v", m.Author.Mention(), randNum, num))
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module goDiceRollerBot
+
+go 1.15
+
+require (
+	github.com/bwmarrin/discordgo v0.23.1
+	github.com/joho/godotenv v1.3.0
+)
